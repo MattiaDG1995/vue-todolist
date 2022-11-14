97ee134483dd4f9e9caafa5d024175fb870fcef9
MattiaDG1995 <mattiadigennaro95@gmail.com>
2022-11-14 14:12:50 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+var app = new Vue({
+    el: '#root',
+    data: {
+        obj: [
+            {id:1, immagine:'./assets/img/imm1.jpg'},
+            {id:2, immagine:'./assets/img/imm2.jpg'},
+            {id:3, immagine:'./assets/img/imm3.jpg'},
+            {id:4, immagine:'./assets/img/imm4.jpg'},
+            {id:5, immagine:'./assets/img/imm5.jpg'}
+        ],
+        todoDone: [],
+    }})
